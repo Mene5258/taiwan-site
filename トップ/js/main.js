3321f4488bbf3b580a58f7131f0d9dd6c73f8841
otadesuyo <o.otajanka@gmail.com>
2024-08-02 11:50:37 +0900

    nu

--- トップ/js/main.js
+++ トップ/js/main.js
@@ -7,12 +7,13 @@ if (window.matchMedia("(max-width: 780px)").matches) {
   btn.forEach(function (item) {
     item.onclick = function () {
       document.getElementById('main_image').src = this.dataset.image;
+
     }
   });
 } else {
   btn.forEach(function (item, index) {
     item.onclick = function () {
-      document.getElementById('main_image').src = this.dataset.image;
+
     }
   });
 
