64f9af9441a1f0ba8dc28f435f7e41f50ffc143c
Mene5258 <irukashou@gmail.com>
2024-08-07 11:52:25 +0900

    台中　途中

--- 台中/js/main.js
+++ 台中/js/main.js
@@ -1,1 +1,55 @@
 'use strict'
+
+// モーダル1
+const dialog1 = document.querySelector('dialog');
+const openbtn1 = document.getElementById('openbtn1');
+
+openbtn1.onclick = function () {
+  dialog1.show();
+};
+
+const closebtn1 = document.getElementById('closebtn1');
+closebtn1.onclick = function () {
+  dialog1.close();
+};
+
+// モーダル２
+
+const dialog2 = document.querySelector('dialog');
+const openbtn2 = document.getElementById('openbtn2');
+
+openbtn2.onclick = function () {
+  dialog2.show();
+};
+
+const closebtn2 = document.getElementById('closebtn2');
+closebtn2.onclick = function () {
+  dialog2.close();
+};
+
+// モーダル3
+const dialog3 = document.querySelector('dialog');
+const openbtn3 = document.getElementById('openbtn3');
+
+openbtn3.onclick = function () {
+  dialog3.show();
+};
+
+const closebtn3 = document.getElementById('closebtn3');
+closebtn3.onclick = function () {
+  dialog3.close();
+};
+
+// モーダル4
+
+const dialog4 = document.querySelector('dialog');
+const openbtn4 = document.getElementById('openbtn4');
+
+openbtn4.onclick = function () {
+  dialog4.show();
+};
+
+const closebtn4 = document.getElementById('closebtn4');
+closebtn4.onclick = function () {
+  dialog4.close();
+};
