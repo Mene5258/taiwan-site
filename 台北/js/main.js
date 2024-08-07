8c9943a1b44edfada44dbe78d23575a3c3f95a49
akkinako <chibibeans@gmail.com>
2024-08-07 13:28:42 +0900

    ｈ

--- 台北/js/main.js
+++ 台北/js/main.js
@@ -1,1 +1,27 @@
 'use strict';
+// モーダル1
+const dialog1 = document.querySelector('.md_1');
+const openbtn = document.querySelector('.mordal_1');
+openbtn.onclick = function () {
+  dialog1.show('.mordal1');
+};
+const closebtn = document.getElementById('closebtn');
+closebtn.onclick = function () {
+  dialog.close();
+};
+
+// モーダル2
+const dialog2 = document.querySelector('.md_2');
+const openbtn2 = document.querySelector('.mordal_2');
+openbtn2.onclick = function () {
+  dialog2.show('.mordal2');
+};
+const closebtn = document.getElementById('closebtn');
+closebtn.onclick = function () {
+  dialog.close();
+};
+// モーダル3
+
+
+
+// モーダル4
